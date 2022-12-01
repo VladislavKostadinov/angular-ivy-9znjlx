619857d212bccfa9906259a9829c31a5c6fcbd30
VladislavKostadinov <79514275+VladislavKostadinov@users.noreply.github.com>
2022-12-01 16:35:42 +0200

    2 files modified

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { TitleService } from './title.service';
 
 @Component({
@@ -18,28 +24,30 @@ export class SimpleFormComponent {
   public wrongTitle = true;
 
   public userForm: FormGroup;
-  public userFirstName = "";
-  public userLastName: any = "";
-  public userTitle = "";
-  public acceptedTerms = "";
-  public selectedOptions = "";
+  public userFirstName = '';
+  public userLastName: any = '';
+  public userTitle = '';
+  public acceptedTerms = '';
+  public selectedOptions = '';
   public lastNameMandatory: any;
-  
-  public submitedForm: boolean = false;
+
+  public submitedForm: boolean = true;
+  public unsubmittedForm: boolean = false;
 
   constructor(private titleService: TitleService, private fb: FormBuilder) {
     this.userForm = this.fb.group({
-      userTitle: "",
-      userFirstName: "",
-      userLastName: "",
-      acceptedTerms: "",
-    })
+      userTitle: '',
+      userFirstName: '',
+      userLastName: '',
+      acceptedTerms: '',
+    });
   }
   ngOnInit() {
-
-    this.lastNameMandatory = new FormControl(this.userForm.get("userLastName").value, 
-      Validators.required);
-      console.log(this.lastNameMandatory)
+    this.lastNameMandatory = new FormControl(
+      this.userForm.get('userLastName').value,
+      Validators.required
+    );
+    console.log(this.lastNameMandatory);
 
     this.titleService
       .getTitles()
@@ -49,41 +57,50 @@ export class SimpleFormComponent {
       console.log(value);
       for (const [keys, val] of Object.entries(value)) {
         this.results.push(val);
-      } }
-      console.log(this.results);
-
-      for (var i = 0; i < this.results.length; i += 2) {
-        this.finalTitlesList.push(this.results[i]);
-        if (!this.results[i].indexOf("!")) {
-          this.finalTitlesList.pop();
-        }
-        this.finalTitlesList.sort();
-      }
-      for (var i = 1; i < this.results.length; i+= 2) {
-        if (this.results[i] === this.wrongTitle) {
-          this.defaultTitle = this.results[i-1];
-        }        
       }
     }
+    console.log(this.results);
 
-      onSubmit(formData) {
-        
-        this.userTitle = formData.userTitle;
-        this.userFirstName = this.userForm.get("userFirstName")?.value;
-        this.userLastName = this.userForm.get("userLastName")?.value;
-        this.acceptedTerms = this.userForm.get("acceptedTerms").value ? this.userForm.get("acceptedTerms")?.value : false ;
-        console.log("User title: " + (this.userTitle ? this.userTitle : this.defaultTitle) + "\n" + "User First Name: " + (this.userFirstName ? this.userFirstName : " Empty") + "\n" + "User Last Name: " + this.userLastName + "\n" + "Accepter Terms: " + this.acceptedTerms);
-        this.submitedForm = true;
-
-
-
+    for (var i = 0; i < this.results.length; i += 2) {
+      this.finalTitlesList.push(this.results[i]);
+      if (!this.results[i].indexOf('!')) {
+        this.finalTitlesList.pop();
       }
-
-      test() {
-        this.lastNameMandatory = new FormControl(this.userForm.get("userLastName").value, 
-      Validators.required);
-      console.log(this.lastNameMandatory.pristine)
+      this.finalTitlesList.sort();
+    }
+    for (var i = 1; i < this.results.length; i += 2) {
+      if (this.results[i] === this.wrongTitle) {
+        this.defaultTitle = this.results[i - 1];
       }
-  
+    }
   }
 
+  onSubmit(formData) {
+    if (!formData.userLastName.length) {
+      this.submitedForm = false;
+    } else {
+      this.submitedForm = true;
+    }
+    this.userTitle = formData.userTitle;
+    this.userFirstName = this.userForm.get('userFirstName')?.value;
+    this.userLastName = this.userForm.get('userLastName')?.value;
+    this.acceptedTerms = this.userForm.get('acceptedTerms').value
+      ? this.userForm.get('acceptedTerms')?.value
+      : false;
+    console.log(
+      'User title: ' +
+        (this.userTitle ? this.userTitle : this.defaultTitle) +
+        '\n' +
+        'User First Name: ' +
+        (this.userFirstName ? this.userFirstName : ' Empty') +
+        '\n' +
+        'User Last Name: ' +
+        this.userLastName +
+        '\n' +
+        'Accepter Terms: ' +
+        this.acceptedTerms
+    );
+
+    console.log(this.submitedForm);
+  }
+}
